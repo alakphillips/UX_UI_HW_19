6c2e8cf99b5e0c20c7bc67b6cafffdeab1921685
alakphillips <alak@ivymedia.org>
2021-05-06 14:40:45 -0500

    update
    update

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log("Your index loaded correctly");
+
+// Smooth Scroll Effect
+$("#about-section").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".about-me").offset().top
+    }, 100);
+});
+$("#skills-section").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".myskills").offset().top
+    }, 100);
+});
+
+$("#work-section").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#projects").offset().top
+    }, 100);
+});
